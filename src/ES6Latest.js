11f156c4bfb66d95d2814f8d32e8afb19d2d3fd0
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-10 01:38:36 +0530

    flat array property

--- src/ES6Latest.js
+++ src/ES6Latest.js
@@ -118,3 +118,12 @@ console.log(
     return accumulator;
   }, {})
 );
+
+
+// JavaScript Arrays
+// The flat() Method
+
+const myArr = [[1,2],[3,4],[5,6]];
+
+const newArr = myArr.flat();
+console.log("Flatted Array",newArr);
