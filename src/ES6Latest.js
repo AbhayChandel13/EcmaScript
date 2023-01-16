1dce584c201ebf63b7831d7b31042ec2641c4f26
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-17 04:49:48 +0530

    comment edited

--- src/ES6Latest.js
+++ src/ES6Latest.js
@@ -99,8 +99,8 @@ console.log(
     .reduce((accumulator, currentValue) => accumulator * currentValue)
 );
 
-//Count elements in array of arrays
-// Count the occurrences of distinct elements in the given 2D array. The given input is an array, the elements of which are arrays of strings. The result is an object whose property names are the values from the arrays and their value is the number of their occurrences.
+//Count elements in array of arrays:
+// Count the occurrences of distinct elements in the given 2D array. The given input is an array, the elements of which are arrays of strings. The result is  an object whose property names are the values from the arrays and their value is the number of their occurrences.
 
 const input5 = [
   ["a", "b", "c"],
