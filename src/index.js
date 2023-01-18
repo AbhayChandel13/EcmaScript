104693b1aad17eabd5a23de3b70c91030faabbe4
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-01-18 20:15:57 +0530

    comments added

--- src/index.js
+++ src/index.js
@@ -195,7 +195,7 @@ spacePeople().then(
 );
 */
 //console.log(cannot oad space people)
-
+//console.log(cannot oad space people)
 const getPeopleInSpace = () =>
   fetch("http://api.open-notify.org/astros.json").then((res) => res.json());
 
