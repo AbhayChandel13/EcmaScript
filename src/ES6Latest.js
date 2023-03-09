cca47c10318b662a3a7c59eb2ce95dac604c9d30
AbhayChandel13 <chandelabhay4515@gmail.com>
2023-03-09 20:08:31 +0530

    comment unedited

--- src/ES6Latest.js
+++ src/ES6Latest.js
@@ -105,7 +105,7 @@ console.log(
 );
 
 //Count elements in array of arrays:
-// Count the occurrences of distinct elements in the given 2D array. The given input is an array,the elements of which are arrays of strings. The result is  an object whose property names are the values from the arrays and their value is the number of their occurrences.
+// Count the occurrences of distinct elements in the given 2D array. The given input is an array, the elements of which are arrays of strings. The result is  an object whose property names are the values from the arrays and their value is the number of their occurrences.
 
 const input5 = [
   ["a", "b", "c"],
