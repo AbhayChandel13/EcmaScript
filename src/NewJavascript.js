0fdb2ac15e67590ca29b4203acf6350c351df48c
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-07-22 20:07:49 +0530

    array values added

--- src/NewJavascript.js
+++ src/NewJavascript.js
@@ -70,7 +70,7 @@ let arr = [1, 2, 3];
 let newArr = arr.map((x) => x * 2); // newArr is [2, 4, 6]
 
 //Index of array from last.
-let arr1 = [1, 2, 3, 2,3, 7];
+let arr1 = [1, 2, 3, 2, 3, 7];
 arr1.lastIndexOf(2); // returns 3
 
 //Index of array
