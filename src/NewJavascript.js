215341af5dc02f4c3941b672496dde6d1112bb90
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-07-29 20:01:58 +0530

    filter array method

--- src/NewJavascript.js
+++ src/NewJavascript.js
@@ -114,3 +114,8 @@ debouncedLogMessage("Hello, world!");
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map(num => num * 2);
 console.log(doubled); // [2, 4, 6, 8]
+
+
+const numbers2 = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers2.filter(num => num % 2 === 0);
+console.log(evenNumbers); // [2, 4, 6]
