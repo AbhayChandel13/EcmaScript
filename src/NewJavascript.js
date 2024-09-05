f36298cac154ed043da1c938a10eae602d80ec6d
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-09-05 20:24:47 +0530

    comments remove

--- src/NewJavascript.js
+++ src/NewJavascript.js
@@ -229,4 +229,4 @@ console.log(port); //output:3000
 
 const port2 = process.env.PORT || 5000;
 console.log(port2); // Output: 5000 (if process.env.PORT is undefined)
-console.log(port2); //output:3000 
+console.log(port2);
