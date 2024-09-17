2dda8a33964ec786973f047b4d8e4085f389cf74
AbhayChandel13 <chandelabhay4515@gmail.com>
2024-09-17 20:16:00 +0530

    remove commented function

--- src/ES6Latest.js
+++ src/ES6Latest.js
@@ -138,10 +138,7 @@ console.log("Flatted Array", newArr);
 // async function myDisplay() {
 //   let myPromise = new Promise(function(resolve) {
 //     setTimeout(function() {resolve("I love You !!");}, 3000);
-//   });
-//   await myPromise
-//    //  document.getElementById("demo").innerHTML = await myPromise;
-// }
+
 
 
 // Today's tasks: 
